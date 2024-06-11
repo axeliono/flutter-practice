32973f2a77321e500e46cfed1fa14a1a8eb983a1
axeliono <69375446+axeliono@users.noreply.github.com>
2024-06-10 22:40:28 -0500

    Creating multiple routes, adding favorites page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'components/big_card.dart';
+import 'pages/generator_page.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -27,20 +30,126 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  var favorites = <WordPair>[];
+  void toggleFavorites() {
+    if (favorites.contains(current)) {
+      print('Removed $current from favorites');
+      favorites.remove(current);
+    } else {
+      print('Favorited $current');
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
-class MyHomePage extends StatelessWidget {
+// ...
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
 
     return Scaffold(
-      body: Column(
+      body: Row(
         children: [
-          Text('a random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                selectedIndex = value;
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
         ],
       ),
     );
   }
+}
+
+// ...  @override
+Widget build(BuildContext context) {
+  var appState = context.watch<MyAppState>();
+  var pair = appState.current;
+
+  IconData icon;
+  if (appState.favorites.contains(pair)) {
+    icon = Icons.favorite;
+  } else {
+    icon = Icons.favorite_border;
+  }
+
+  return Scaffold(
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorites();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like')),
+              SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print('button pressed');
+                    appState.getNext();
+                  },
+                  child: Text('Next')),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
 }
